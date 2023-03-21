2ab0322705fa5f29e4027918eaa7c27fc7b55d43
MaysVerrecas <99118212+MaysVerrecas@users.noreply.github.com>
2023-03-21 15:30:51 +0300

    first commit

---
+++ src/main/java/com/example/jbdcproject/JbdcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jbdcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbdcProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbdcProjectApplication.class, args);
+    }
+
+}
